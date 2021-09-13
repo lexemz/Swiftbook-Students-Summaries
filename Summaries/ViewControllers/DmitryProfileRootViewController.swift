d5859dd34cdc3ff92674b9c3ff03f0bb45a7a8a5
Dmitriy Danilin <danilindn@icloud.com>
2021-09-13 14:34:10 +0300

    refactor: add mark

--- Summaries/ViewControllers/DmitryProfileRootViewController.swift
+++ Summaries/ViewControllers/DmitryProfileRootViewController.swift
@@ -9,12 +9,14 @@ import UIKit
 
 class DmitryProfileRootViewController: UIViewController {
     
+    // MARK: - Outlets
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var fullNameLabel: UILabel!
     @IBOutlet weak var professionLabel: UILabel!
     @IBOutlet weak var skillsLabel: UILabel!
     @IBOutlet weak var moreInfoButton: UIButton!
     
+    // MARK: - Private properties
     private let person = Person.getPerson()
     
 
@@ -41,7 +43,7 @@ class DmitryProfileRootViewController: UIViewController {
 
 }
 
-// MARK: - Private method
+// MARK: - Default method setup
 extension DmitryProfileRootViewController {
     private func setupPhotoImageView() {
         photoImageView.image = UIImage(named: "dmitriyPhoto")
