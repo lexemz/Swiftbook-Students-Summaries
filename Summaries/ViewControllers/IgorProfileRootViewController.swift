435812e87375d8e8422de73928944354a0eff181
Igor Buzykin <lexemz@gmail.com>
2021-09-14 16:30:13 +0300

    create storyboard reference

--- Summaries/ViewControllers/IgorProfileRootViewController.swift
+++ Summaries/ViewControllers/IgorProfileRootViewController.swift
@@ -11,7 +11,7 @@ class IgorProfileRootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = UIColor.systemPink
         // Do any additional setup after loading the view.
     }
     
