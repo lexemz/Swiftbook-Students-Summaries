a2e9274b756f2672aa25a87311972eeaaf7f271b
Alex <arcani91@gmail.com>
2021-09-15 22:51:01 +0300

    Set up the NavVC + background image!

--- Summaries/ViewControllers/AlexProfileRootViewController.swift
+++ Summaries/ViewControllers/AlexProfileRootViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class AlexProfileRootViewController: UIViewController {
+class AlexProfileRootViewController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
