b1f837b7d7aeb7ff45e46ea3eea5623f351f4cc3
Dmitriy Danilin <danilindn@icloud.com>
2021-09-16 15:56:05 +0300

    chore: set the number of cells

--- Summaries/ViewControllers/ProjectListDNViewController.swift
+++ Summaries/ViewControllers/ProjectListDNViewController.swift
@@ -20,15 +20,8 @@ class ProjectListDNViewController: UITableViewController {
     }
 
     // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        projects.count
     }
 
     /*
