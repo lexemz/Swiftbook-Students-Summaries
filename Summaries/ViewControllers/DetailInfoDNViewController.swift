9d813e18c42245a4584f4ed974d5dd074960a64e
Dmitriy Danilin <danilindn@icloud.com>
2021-09-16 15:50:55 +0300

    refactor: delete commented code

--- Summaries/ViewControllers/DetailInfoDNViewController.swift
+++ Summaries/ViewControllers/DetailInfoDNViewController.swift
@@ -40,7 +40,11 @@ class DetailInfoDNViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+//    @IBAction func testLink() {
+//        if let url = URL(string: person.profession.projects[0].linkGitHub!) {
+//            UIApplication.shared.open(url)
+//        }
+//    }
 }
 
 // MARK: - Default setup methods
