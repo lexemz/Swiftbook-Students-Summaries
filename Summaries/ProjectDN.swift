b71f6f927939d6e72b8dfe20fa1ac1e721539655
Dmitriy Danilin <danilindn@icloud.com>
2021-09-16 14:37:46 +0300

    refactor: edit model, project
    Del: link to App Store

--- Summaries/ProjectDN.swift
+++ Summaries/ProjectDN.swift
@@ -8,18 +8,15 @@
 struct Project {
     let projectName: String
     let projectDescription: String
-    let linkAppStore: String?
     let linkGitHub: String?
     
     static func getProjects() -> [Project] {
         [
             Project(projectName: "Two Views",
                     projectDescription: "Учебное приложения. Практика работы с констрейнтами",
-                    linkAppStore: nil,
                     linkGitHub: "https://github.com/ZyFun/TwoViews.git"),
             Project(projectName: "Traffic Light",
                     projectDescription: "Учебное приложение. Имитирует работу светофора в ручном управлении",
-                    linkAppStore: nil,
                     linkGitHub: "https://github.com/ZyFun/TrafficLight.git")
         ]
     }
