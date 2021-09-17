e6e1687b65f67f7e283eb91c52932c3df70f3cc8
Igor Buzykin <lexemz@gmail.com>
2021-09-17 17:26:22 +0300

    u: tableview link trigger

--- Summaries/ViewControllers/IgorProfileRootViewController.swift
+++ Summaries/ViewControllers/IgorProfileRootViewController.swift
@@ -23,12 +23,11 @@ class IgorProfileRootViewController: UIViewController {
         
         tableView.delegate = self
         tableView.dataSource = self
-        
-        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
-    override func viewDidAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) {
         navigationController?.topViewController?.title = userData.fullName
+        navigationController?.navigationBar.prefersLargeTitles = true
     }
     
     override func viewDidDisappear(_ animated: Bool) {
@@ -41,23 +40,25 @@ class IgorProfileRootViewController: UIViewController {
 
 extension IgorProfileRootViewController: UITableViewDelegate, UITableViewDataSource {
     func numberOfSections(in tableView: UITableView) -> Int {
-        4
+        5
     }
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         switch section {
         case 0: return nil
-        case 1: return "Хобби"
-        case 2: return "Навыки"
+        case 1: return "Контакты"
+        case 2: return "Хобби"
+        case 3: return "Навыки"
         default: return "Проекты"
         }
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         switch section {
-        case 0: return 3
-        case 1: return userData.hobbies.count
-        case 2: return userData.skills.count
+        case 0: return 2
+        case 1: return userData.contacts.count
+        case 2: return userData.hobbies.count
+        case 3: return userData.skills.count
         default: return userData.projects.count
         }
     }
@@ -71,24 +72,26 @@ extension IgorProfileRootViewController: UITableViewDelegate, UITableViewDataSou
             cell.selectionStyle = .none
             if indexPath.row == 0 {
                 content.text = "Age: \(userData.age)"
-            } else if indexPath.row == 1 {
-                content.text = "Telegram: \(userData.contacts)"
-            } else if indexPath.row == 2 {
+            } else {
                 content.text = "City: \(userData.city)"
             }
         case 1:
+            cell.accessoryType = .disclosureIndicator
+            let contact = userData.contacts[indexPath.row]
+            content.text = "\(contact.contactType): \(contact.id)"
+        case 2:
             cell.selectionStyle = .none
             content.text = userData.hobbies[indexPath.row].name
-        case 2:
+        case 3:
             cell.selectionStyle = .none
             let skill = userData.skills[indexPath.row]
             content.text = skill.title
             content.secondaryText = "Level: \(skill.skillLevel.rawValue)"
         default:
+            cell.accessoryType = .disclosureIndicator
             let project = userData.projects[indexPath.row]
             content.text = project.title
             content.secondaryText = project.descrition
-            cell.accessoryType = .disclosureIndicator
         }
         
         cell.contentConfiguration = content
@@ -96,8 +99,11 @@ extension IgorProfileRootViewController: UITableViewDelegate, UITableViewDataSou
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-//        print(indexPath.section, indexPath.row)
-        if indexPath.section == 3 {
+        if indexPath.section == 2 {
+            let contacts = userData.contacts
+            guard let url = URL(string: contacts[indexPath.row].link) else { return }
+            UIApplication.shared.open(url)
+        } else if indexPath.section == 4 {
             let projects = userData.projects
             guard let url = URL(string: projects[indexPath.row].link) else { return }
             UIApplication.shared.open(url)
