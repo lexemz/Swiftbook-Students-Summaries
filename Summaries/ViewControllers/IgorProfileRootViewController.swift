2a5ddd8d7ffc6d7d9bcbe8d5133519c56d4f7d1e
Igor Buzykin <lexemz@gmail.com>
2021-09-17 16:22:53 +0300

    default tableView configuration

--- Summaries/ViewControllers/IgorProfileRootViewController.swift
+++ Summaries/ViewControllers/IgorProfileRootViewController.swift
@@ -9,10 +9,13 @@ import UIKit
 
 class IgorProfileRootViewController: UIViewController {
 
+    @IBOutlet var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        view.backgroundColor = UIColor.systemPink
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -25,15 +28,19 @@ class IgorProfileRootViewController: UIViewController {
         navigationController?.navigationBar.prefersLargeTitles = true
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension IgorProfileRootViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
     }
-    */
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IgorInfoCell", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        content.text = "test"
+        
+        cell.contentConfiguration = content
+        return cell
+    }
 }
